9ed6c10cfd61d8ae94e1e443e4e42d1a0d065dae
Jakelr26 <146326562+Jakelr26@users.noreply.github.com>
2024-04-28 21:28:21 +0100

    el fin

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -58,55 +58,20 @@ class Program
 {
 
     /// THIS LINE: If you want to add methods add them between THIS LINE and THAT LINE
-    static bool NegativeSearchTree(Node tree, DataEntry value)
-    {
-        //  Fill in proper code
-
-        Node valueNode = new Node();
-        valueNode.data = value;
-
-        if (IsEqual(tree, valueNode))
-        {
-            return false;
-        }
-
-
-        if (IsSmaller(tree, valueNode))
-        {
-            if (tree.right == null)
-            {
-                return true;
-            }
-            else
-            {
-                return SearchTree(tree.right, value);
-            }
-
-        }
-
-        if (IsSmaller(valueNode, tree))
-        {
-
-            if (tree.left == null)
-            {
-                return true;
-            }
-            else
-            {
-                return SearchTree(tree.left, value);
-
-            }
-
-
-        }
-
-        return false;
-    }
+    
 
 
     /// Your methods go here  .... (and nowhere else)
     /// 
-
+    
+    
+    /// <summary>
+    /// Deletes parts of the tree
+    /// checks for childrem and acts accordingly
+    /// replaces children in correct order
+    /// </summary>
+    /// <param name="item">to delete</param>
+    /// <param name="root">root of tree</param>
     static Node DeleteItemPart(Node root, Node item)
     {
         if (root != null)
@@ -147,7 +112,8 @@ class Program
 
     }
     /// <summary>
-    /// 
+    /// merges two trees, results in one with no duplicates
+    /// uses recursion to pass tree
     /// </summary>
     /// <param name="Union">where the compiled list is stored</param>
     /// <param name="InTree">ofiginal tree</param>
@@ -177,7 +143,8 @@ class Program
 
     }
     /// <summary>
-    /// 
+    /// finds the intersection of the of two trees (sets)
+    /// recurses through to find all the data
     /// </summary>
     /// <param name="direcion"></param>
     /// <param name="tree"></param>
@@ -207,7 +174,14 @@ class Program
             
         }
     }
-    
+    /// <summary>
+    /// Find the symetric difference of two trees that represent sets. 
+    /// uses intersection and the union
+    /// Search tree is used to find the specific nodes in the tree, and recursion is used to change the Value of the part being searched for
+    /// </summary>
+    /// <param name="direcionallroot"></param>
+    /// <param name="intersect"></param>
+    /// <param name="results"></param>
     static void TreeDiff(Node direcionallroot, Node intersect, Tree results)
     {
         if (direcionallroot != null && intersect != null)
@@ -230,7 +204,14 @@ class Program
         }
         
     }
-
+    /// <summary>
+    /// Finds the asymetricall difference of two trees
+    /// searches for data in tree2 and if it finds it it doesnt add to the result tree,
+    /// but if doesnt, it measn they dont match and it gets added to the tree
+    /// </summary>
+    /// <param name="root1"></param>
+    /// <param name="tree2"></param>
+    /// <param name="resultTree"></param>
     static void NormalDifference(Node root1, Node tree2, Tree resultTree)
     {
         if (root1 == null)
@@ -364,7 +345,7 @@ class Program
     /// <param name="item">The Node to insert</param>
     static void InsertTree(Tree tree, Node item)
     {
-        InsertItem(ref tree.root, item);
+        InsertItem(ref tree.root, item); // using insert item, but with different parameters.
 
     }
 
@@ -383,16 +364,16 @@ class Program
     {
         //  Fill in proper code
 
-        Node valueNode = new Node(); 
+        Node valueNode = new Node(); //makes node 
         valueNode.data = value;
 
-        if (IsEqual(tree, valueNode))
+        if (IsEqual(tree, valueNode)) // checks if tree (item being searched for) is in the tree, if yes return true
         {
             return true;
         }
 
 
-        if (IsSmaller(tree, valueNode))
+        if (IsSmaller(tree, valueNode)) //finds if the node is too the right, if yes recurses to the right
         {
             if (tree.right == null)
             {
@@ -405,7 +386,7 @@ class Program
 
         }
 
-        if (IsSmaller(valueNode, tree))
+        if (IsSmaller(valueNode, tree)) //finds if the node is too the left, if yes recurses to the left
         {
 
             if (tree.left == null)
@@ -439,17 +420,17 @@ class Program
     static bool SearchTreeItem(Node tree, Node item)
     {
 
-        if (tree == null || item == null)
+        if (tree == null || item == null) // checks that their is data in the perameteres 
         {
             return false;
         }
 
-        if (IsEqual(tree, item))
+        if (IsEqual(tree, item))  // uses isEqual function to see if the item is in the tree
         {
             return true;
         }else
         {
-            if (IsSmaller(tree, item))
+            if (IsSmaller(tree, item))  // searches to see if the item is to the right and then will recurse to the right
             {
                 if (tree.right == null)
                 {
@@ -460,7 +441,7 @@ class Program
                     return SearchTreeItem(tree.right, item);
                 }
             }
-            if (!IsSmaller(tree, item))
+            if (!IsSmaller(tree, item)) // checks to see if the item is too left and then will recurse to the left 
             {
                 if (tree.left == null)
                 {
@@ -474,7 +455,7 @@ class Program
             }
         }
 
-        return false;
+        return false; // returns false if after all recursion, bnothing is found
 
     }
 
@@ -486,7 +467,7 @@ class Program
     /// <param name="item">The Node to remove</param>
     static void DeleteItem(Tree tree, Node item)
     {
-        tree.root = DeleteItemPart(tree.root, item);
+        tree.root = DeleteItemPart(tree.root, item); // calls my delete part function/ method 
     }
     
 
@@ -498,16 +479,16 @@ class Program
     static int Size(Tree tree)
     {
 
-        if (tree.root == null)
+        if (tree.root == null) // if no tree, theres no nodes
         { return 0; }
 
-        Tree Left = new Tree();
-        Left.root = tree.root.left;
+        Tree Left = new Tree();  // making a left tree
+        Left.root = tree.root.left;  //  sets root to left read for recusrsion 
 
-        Tree Right = new Tree();       
-        Right.root = tree.root.right;
+        Tree Right = new Tree(); //making a right tree
+        Right.root = tree.root.right;  // sets root to right ready for recursion
 
-        if (tree != null)
+        if (tree != null) // if the tree exists, will recurse+ add 1 for the top node
         { return 1 + Size(Left) + Size(Right); }
         else
         { return 0; }
@@ -523,32 +504,32 @@ class Program
         /// <param name="tree">The root of the tree</param>
         /// <returns>The depth of the tree.</returns>
         static int Depth(Node tree)
-    {
-
-        if (tree != null) 
         {
+
+            if (tree != null) // will only run if a tree has been parsed in 
+            {
          
-            int leftDepth = Depth(tree.left);
+                int leftDepth = Depth(tree.left); //recurses through to the left 
 
-            int rightDepth = Depth(tree.right);
+                int rightDepth = Depth(tree.right); // recurses through to the right
             
-            if (leftDepth > rightDepth)
+                if (leftDepth > rightDepth) // checks which one is deepest, then will adds the root node + the depth and return it
             {
-                return 1 + leftDepth;
+                    return 1 + leftDepth;  // 
+                } 
+                else
+                {
+                    return 1 + rightDepth;
+                }
+
+
             } 
-            else
-            {
-                return 1 + rightDepth;
-            }
-
-
-        } 
-        else { return 0; }
+            else { return 0; } // if no tree, no depth
 
         
 
        
-    }
+        }
 
 
     /// <summary>
@@ -560,20 +541,20 @@ class Program
     static Node Parent(Tree tree, Node node)
     {
         
-        if (tree.root == null || node == null)
+        if (tree.root == null || node == null) // makes sure params exist
         { return null; }
 
-        Tree Left = new Tree();
+        Tree Left = new Tree(); //making trees for left and right, and sets them up ready for the recursion
         Left.root = tree.root.left;
         Tree Right = new Tree();
         Right.root = tree.root.right;
 
 
-        if (Left.root == null && Right.root == null)
+        if (Left.root == null && Right.root == null) // checks for their existence,
         {
             return null;
         }
-        if (Right.root != null)
+        if (Right.root != null) //  then the next two if statements will check if the next node is the child and if so, they will return the parent
         {
             if (Right.root.data.data == node.data.data)
             {
@@ -587,11 +568,7 @@ class Program
                 return tree.root;
             }
         }
-     
-        
-        
-
-        if (tree != null)
+        if (tree != null)  // recursive functions dependent on if the parent is to the left or the right, and will uses the Trees at the top as params
         {
             if (node.data.data > tree.root.data.data)
             {
@@ -605,7 +582,7 @@ class Program
         }
 
 
-        return null;
+        return null; // no trees, then no parents
     }
 
 
@@ -616,10 +593,10 @@ class Program
     /// <returns>The Node that contains the largest value in the sub-tree provided.</returns>
     static Node FindMax(Node tree)
     {
-        if (tree!= null)
+        if (tree!= null) // checks if there is a tree
         {
 
-            while (tree.right != null)
+            while (tree.right != null) // will set the root of the tree to the right untill it finds the rightmost value which is hence the largest 
             {
                 tree = tree.right;
             }
@@ -627,7 +604,7 @@ class Program
             
         } else
         {
-            return null;
+            return null; // if their is no tree, then theres no max node
         }
                     
     }
@@ -639,28 +616,25 @@ class Program
     /// <param name="tree">The Tree to process.</param>
     static void DeleteMin(Tree tree)
     {
-        if (tree != null)
+        if (tree != null) // checks if their is a tree
         {
-            Node current = tree.root;
+            Node current = tree.root; // sets up a Node based on the Tree, to get around the parameter being a tree
 
-            if (current.left != null)
+            if (current.left != null) 
             {
-                while (current.left != null)
+                while (current.left != null) // loops untill the left most (smallest) value  is foud
                 {
                     current = current.left;
                 }
             }
 
-            if (current.left == null)
+            if (current.left == null) 
             {
-                Console.WriteLine();
+                Console.WriteLine(); // function is a void, and wanted to use data to show what is beind deleted, and so it made sense to write them in the function
                 Console.WriteLine("****************DeleteMinimumValue*****************");
                 Console.WriteLine("Deleting minumum value, of number: " + current.data.data);
 
-                tree.root = DeleteItemPart(tree.root, current);
-
-
-
+                tree.root = DeleteItemPart(tree.root, current); // goes to an exterior function
             }
         }
     }
@@ -678,12 +652,12 @@ class Program
     /// <returns>A new tree with all the values from tree1 and tree2.</returns>
     static Tree Union(Tree tree1, Tree tree2)
     {
-        if ((tree1 != null) && (tree2 != null))
+        if ((tree1 != null) && (tree2 != null)) // checks  that there are actully two trees 
         {
-            Tree treeUnion = new Tree();
-            treeMerger(treeUnion, tree2.root);
+            Tree treeUnion = new Tree(); // makes a tree for the union to be put in 
+            treeMerger(treeUnion, tree2.root);// two links to an external function that will merge the trees together and store in treeunion
             treeMerger(treeUnion, tree1.root);
-            return treeUnion;
+            return treeUnion; //returns the tree of merged trees
         }
 
         return null;
@@ -698,21 +672,21 @@ class Program
     /// <returns>A new Tree with all values in tree1 and tree2.</returns>
     static Tree Intersection(Tree tree1, Tree tree2)
     {
-        if ((tree1 == null) || (tree2 == null))
+        if ((tree1 == null) || (tree2 == null)) // if trees dont exit, fucntion returns null 
         {
             return null;
 
         }
         else
         {
-            Tree treeUnion = new Tree();
+            Tree treeUnion = new Tree(); // two new trees, to store the union on that will be subracted uppon, and the results which is the tree to be returned 
             Tree results = new Tree();
-            treeMerger(treeUnion, tree2.root);
+            treeMerger(treeUnion, tree2.root); 
             treeMerger(treeUnion, tree1.root);
 
 
-            Intersect(tree1.root,tree2.root, treeUnion, results);
-            return results;
+            Intersect(tree1.root,tree2.root, treeUnion, results);// links to a function that uses the data to recurse and fidn the intersect. 
+            return results;// sends backl the result tree
         }
     }
 
@@ -723,15 +697,15 @@ class Program
     /// <param name="tree1">Tree one</param>
     /// <param name="tree2">Tree two</param>
     /// <returns>The values of the set difference tree1/tree2 in a new Tree.</returns>
-    static Tree Difference(Tree tree1, Node tree2)
+    static Tree Difference(Tree tree1, Node tree2) 
     {
-        if (tree1 != null && tree2 != null)
+        if (tree1 != null && tree2 != null) //only runs if params exist 
         {
-            Tree resultTree = new Tree();
-            NormalDifference(tree1.root, tree2, resultTree);
-            return resultTree;
+            Tree resultTree = new Tree(); // makes tree
+            NormalDifference(tree1.root, tree2, resultTree);// uses tree and the perameteres to find the difference
+            return resultTree;// sends back to initial call 
         }
-        else { return null; }
+        else { return null; } // no tree, no difference 
         
     }
 
@@ -744,27 +718,27 @@ class Program
     /// <returns>The values of the symmetric difference tree1/tree2 in a new Tree.</returns>
     static Tree SymmetricDifference(Node tree1, Tree tree2)
     {
-        if (tree2 == null || tree1 == null)
+        if (tree2 == null || tree1 == null) // if no trees, no run
         {
             return null;
         }
-        Tree treeUnion = new Tree();
+        Tree treeUnion = new Tree(); //making some trees to transfer data into functions 
         Tree t2 = new Tree();
         t2.root = tree1;
         
         if ((tree2 != null) && (tree1 != null))
         {
-            treeUnion = Union(tree2, t2);
+            treeUnion = Union(tree2, t2); // find the union as vital for the maths
          
         }
         
-        Tree inter = Intersection(tree2, t2);
+        Tree inter = Intersection(tree2, t2); // find the intersection 
 
-        Tree results = new Tree();
+        Tree results = new Tree(); // to store results
 
-        TreeDiff(treeUnion.root, inter.root, results);
+        TreeDiff(treeUnion.root, inter.root, results); // function to do the maths 
 
-        return treeUnion;
+        return treeUnion; // sends back to the original call 
     }
 
 
@@ -827,21 +801,28 @@ class Program
 
 
         //  Add more tree testing here .... 
+        
+        // formatting is used on the text to sepparate out the different functions and make the output more readable 
+        // most of this is calling the function and printing it
         Console.WriteLine("");
         Console.WriteLine("**********************Size*************************");
         Console.WriteLine("Size of/ Number of elements in Tree: " + (Size(tree)));
+        
+        
         Console.WriteLine("");
         Console.WriteLine("**********************Depth************************");
         Console.WriteLine("depth of the tree is: " + Depth(tree.root));
+        
+        
         Console.WriteLine("");
         Console.WriteLine("*******************ParentNodes*********************");
         Node parentNode = new Node();
         parentNode.data = data;
 
-        Console.WriteLine("Top of tree = " + tree.root.data.data);
+        Console.WriteLine("Top of tree = " + tree.root.data.data); // saying what the root of the tree is 
         DataEntry DoesntExist = new DataEntry();
         
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++) // loops through one to ten, looking for if a node exists, and if it does, it will print its value and its parent
         {
             parentNode.data.data = i;
             if ((Parent(tree, parentNode) != null))
@@ -850,13 +831,15 @@ class Program
                 Console.WriteLine("");
             }
         }
+
+
         Console.WriteLine("");
         Console.WriteLine("*******************LargestNode*********************");
         Node maxNode = FindMax(tree.root);
         Console.WriteLine("The largest node is " + maxNode.data.data);
 
-
-
+        Console.WriteLine("");
+        Console.WriteLine("****************DeleteRandomValue******************");
         Node nodeDelete = new Node();
         nodeDelete.data = hold.data;
         Console.WriteLine("deleting node with value: " + nodeDelete.data.data);
@@ -864,7 +847,8 @@ class Program
         Console.WriteLine("Print out the (new) tree");
         PrintTree(tree.root);
         Console.WriteLine();
-        
+
+        // Deleting the minimum value
         DeleteMin(tree);
         Console.WriteLine("Print out the (new) tree");
         PrintTree(tree.root);
@@ -895,6 +879,8 @@ class Program
 
         Console.WriteLine("*Build two trees inserting 10 random values as data*");
 
+        Console.WriteLine();
+
         //make tree1
         for (int i = 1; i <= 10; i++)
         {
@@ -917,7 +903,7 @@ class Program
             current.data = data;
             InsertItem(ref tree2.root, current);
         }
-        Console.WriteLine("Tree1:");
+        Console.WriteLine("Tree1:"); // printing out the two trees for this section 
         PrintTree(tree1.root);
         Console.WriteLine();
         Console.WriteLine("Tree2:");
